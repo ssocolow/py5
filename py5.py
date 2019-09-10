fb2f08955795a069e55cc2a6574ae1ed3867db1c
Simon Socolow <simonsocolow78@gmail.com>
2019-09-10 07:44:16 -0400

    Adding comments

--- py5.py
+++ py5.py
@@ -1,28 +1,36 @@
+#importing tkinter
 import tkinter as tk
 
+#initialize global variables
 mainwindow = 0
 canvas = 0
 width = 0
 height = 0
 
+#make a canvas function
 def createCanvas(w, h):
+    #use global variables
     global mainwindow
     global width
     global height
     global canvas
-
+    
+    #make the window
     mainwindow = tk.Tk()
     
     width = w
     height = h
-
+    
+    #make the canvas with the correct width and height in the window
     canvas = tk.Canvas(mainwindow, width=width, height=height)
-
+    
+    #pack canvas (this helps the geometry organizer)
     canvas.pack()
     return canvas
 
-
+#make a line function
 def line(x0, y0, x1, y1, color='blue'):
+    #use global variables
     global canvas
     global mainwindow
     
@@ -30,7 +38,9 @@ def line(x0, y0, x1, y1, color='blue'):
     mainwindow.update()
     return id
 
+#make an ellipse function
 def ellipse(x0, y0, x1, y1, color='blue'):
+    #use global variables
     global canvas
     global mainwindow
     
@@ -38,7 +48,9 @@ def ellipse(x0, y0, x1, y1, color='blue'):
     mainwindow.update()
     return id
 
+#make a rectangle function
 def rect(x0, y0, x1, y1, color='blue'):
+    #use global variables
     global canvas
     global mainwindow
     
@@ -46,21 +58,10 @@ def rect(x0, y0, x1, y1, color='blue'):
     mainwindow.update()
     return id
 
+#update the screen
 def draw():
     mainwindow.update()
 
+#mainloop enters an infinite loop of drawing (useful for static and non responsive graphics) (I think)
 def show():
     mainwindow.mainloop()
-
-
-#createCanvas(500,500)
-#line(0,500,500,0)
-#rect(25,200,80,300)
-#ellipse(200,50,300,450)
-
-
-#x = 500
-#while(x > 0):
- #   line(x,500,500,0, 'blue')
-  #  x -= 1
-   # time.sleep(0.01)
